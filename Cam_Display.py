757c9dd35e32abbfeee67777f14074635c36d1a5
rjohn92 <rolandjohn92@gmail.com>
2022-10-22 13:27:03 -0400

    "Added func to check cam availability"

--- Cam_Display.py
+++ Cam_Display.py
@@ -1,7 +1,6 @@
 """This file will display our webcam footage. We must import cv2 for the display to work."""
 from datetime import datetime
 from calendar import c
-from xmlrpc.client import DateTime
 import cv2
 
 
@@ -10,25 +9,23 @@ filename = str(datetime.now())+'.mp4'
 global  NUMBER
 NUMBER=0
 class Camera:
-    def camera_amount():
-        '''Returns int value of available camera devices connected to the host device'''
-        while True:
-            if (cv2.VideoCapture(NUMBER).grab()) is True:
-                NUMBER+=1
-            elif NUMBER ==0:
-                print("No cameras connected.")
-            else:
-                cv2.destroyAllWindows()
-                return int(NUMBER)
+    def check_cam(NUMBER):
+        cam = cv2.VideoCapture(NUMBER) 
+        if not cam.isOpened():
+            print('Warning! Camera '+ str(NUMBER)+" is unavailable.")
+            return False
+        else:
+            return True
+
                 
     def live_feed(NUMBER):
+        if not Camera.check_cam(NUMBER):
+            return Camera.check_cam(NUMBER)
         """Display function to show our webcam footage. No arguments are passed. To display our webcam 
         footage for our current selected camera simply pass display()."""
         #this will be our initialized webcam feed starting from 0 because we only have
         #one webcam currently in service. For each successive webcam we will increment
         #the cv2.VideoCapture size.
-        feed = cv2.VideoCapture(NUMBER)
-        
         feed = cv2.VideoCapture(NUMBER)
         disabledpngpath = r'camdisabled.png'
         disabledpng = cv2.imread(disabledpngpath)
@@ -44,16 +41,16 @@ class Camera:
             feed.set(4,480)
 
         make_480p(feed)
-        out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('m','p','4','v'), 24, (640,480))
+        out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc('m','p','4','v'), 12, (640,480))
         while True:
             ret, display = feed.read()
-            out.write(display)
+            
             #will display our current webcam feed with the current date and time in the 
             #center of the window.
             cv2.rectangle(display, (195, 32), (427, 12), (0, 0, 0), -1)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(display,str(datetime.now()),(210,25), font, .4,(390,255,255),1,cv2.LINE_AA)
-            
+            out.write(display)
             if(disableWebcam):
                 cv2.imshow('Webcam '+str(NUMBER+1), disabledpng)
             else:
@@ -69,15 +66,19 @@ class Camera:
             if cv2.waitKey(20)==27:
                 disableWebcam = 1 - disableWebcam            
         #this will release our resource so that another program can use our camera.
-        feed.release()
         out.release()
+        feed.release()
+        
         #when we exit the script we can destroy the windows
         cv2.destroyAllWindows()   
 
 
 #this will run our display
 if __name__ =="__main__":
-    Camera.live_feed(NUMBER)
+    Camera.live_feed(0)
+    Camera.live_feed(5)
+    
+
 
 
     
