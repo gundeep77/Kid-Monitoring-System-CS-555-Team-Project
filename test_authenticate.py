cce3ee7e3be61e4e683c0e2df088df9bab7aef96
Mattoulou <mathieumounounga@gmail.com>
2022-11-09 14:46:37 -0500

    Added test cases for authenticate function

---
+++ test_authenticate.py
@@ -0,0 +1,34 @@
+
+import unittest
+from authenticate import authenticate
+import pyotp
+import time
+
+class TestAuthenticate(unittest.TestCase):
+
+    def test_authenticate (self) -> None:
+        "test authenticate function with differents inputs "
+
+        totp = pyotp.TOTP('base32secret3232')
+
+        # arbritary wrong input with 10 digit 
+        user_input1 = '0000000001'
+
+        self.assertEqual(authenticate(user_input1), False)
+
+        # remplace user_input1 with correct input which must have 6 digits
+        user_input2 = totp.now()
+
+        self.assertEqual(authenticate(user_input2), True)
+
+        time.sleep(30)
+
+        # verify user_input2 after 30 secondes
+        self.assertEqual(totp.verify(user_input2), False)
+
+        # input_user2 is no more valid 
+        self.assertEqual(authenticate(user_input2), False)
+       
+    
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
