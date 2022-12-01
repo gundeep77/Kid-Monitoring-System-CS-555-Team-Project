323edfa16d53e4790f44aa48b21de4a43e670c29
Omar Abdelmotaleb <oabdelmo@stevens.edu>
2022-12-01 03:04:49 -0500

    Adding logger

---
+++ logger.py
@@ -0,0 +1,47 @@
+import os
+from datetime import datetime
+
+class Logger:
+    """
+    **Description**:
+    
+    Class designed for logging events on the app.
+
+    """
+
+    # filepath is the path of the file
+    def __init__(self, filepath):
+        self.filepath = filepath
+    
+    # Logs an event, the variable event is a string
+    def log(self, event):
+        with open(self.filepath, "a") as f:
+            f.write(f"[{datetime.now()}] {event} \n")
+            f.close()
+
+
+# File path, needs to have a log.txt in there or some file that can be opened.
+desktop  = os.path.expanduser("~\\Desktop\\Baby Camera Footage") + str("\\log.txt")
+l = Logger(desktop)
+
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
+l.log("This is an event")
